4ed924d9675bd620a176f39bbd4be3c1b16818f6
“pshingsl” <w455ok@gmail.com>
2025-07-07 18:51:46 +0900

    data.js데이터들 ts로 변환 숙제 home.ts

--- ex0707/simple_vote_app/src/home.ts
+++ ex0707/simple_vote_app/src/home.ts
@@ -5,11 +5,32 @@ interface initialTodos {
   isCompleted: boolean;
 }
 
+// initialTodos
 let todo_a: initialTodos = {
   id:1,
   title: "HTML 기본 태그 복습",
   description: "p, h1~h6, ul 등",
   isCompleted: true
 }
-
 console.log(todo_a)
+
+let todo_b: initialTodos = {
+  id:2,
+  title: "Flexbox 연습",
+  isCompleted: true
+}
+
+console.log(todo_b)
+
+//initialUsers
+interface initialUsers{
+  email: string;
+  password:string;
+}
+
+let user1 : initialUsers = {
+  email:"user1@example.com",
+  password: "password123"
+}
+
+const delay = (ms : number ) => new Promise(resolve => setTimeout(resolve, ms));
